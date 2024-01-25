68d8417ef2b0fd78fda320f9db6bef466c1b5b57
HarishBarathiS <harish.sivasubramanian04@gmail.com>
2024-01-25 20:24:00 +0530

    testing find cluster func

--- kmeans.py
+++ kmeans.py
@@ -11,10 +11,21 @@ def find_closest_centroids(X,centroids):
     for i in range(X.shape[0]):
         distance = []
         for j in range(centroids.shape[0]):
-            euclidean_norm = np.sqrt(np.sum(X[i] - centroids[j])**2,axis=1)
+            euclidean_norm = np.sqrt(np.sum(X[i] - centroids[j])**2)
             distance.append(euclidean_norm)
         # argmin will give the index of min value over the axis
         idx[i] = np.argmin(distance) 
     """ idx holds the index of the closest cluster
     for each training example """
     return idx
+
+
+X = np.random.uniform(1,101,size = (100,2))
+print(X[:5])
+
+random_indices = np.random.choice(X.shape[0],size=3,replace=False)
+random_points = X[random_indices]
+""" selecting random 3 points from training example as initial centroids"""
+centroids = random_points
+idx = find_closest_centroids(X,centroids)
+print("First 3 elements in idx : ", idx[:3])
